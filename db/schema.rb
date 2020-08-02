7a8fd38c55d753b4acd4dc02eac3edf312c683db
Buyaki <rittahbuyaki@gmail.com>
2020-08-03 02:21:34 +0300

    Create connection between the user and the tweeets

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200802205008) do
+ActiveRecord::Schema.define(version: 20200802224628) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
